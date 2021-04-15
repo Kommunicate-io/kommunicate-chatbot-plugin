4f768dbfa3cff8d3a917b5b7aa5a46bdedcedfe5
Abhishek Singh <aks3555@gmail.com>
2021-04-15 15:51:27 +0530

    update code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const Kommunicate = (appId) => {
+          let d = window.top.document;
+          let m = window.top.kommunicate;
+          let kommunicateSettings = 
+              {"appId":`${appId}`,"popupWidget":true,"automaticChatOpenOnNavigation":true};
+          let s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+          s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+          let h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+          window.kommunicate = m; m._globals = kommunicateSettings;
+
 }
+export default Kommunicate;
