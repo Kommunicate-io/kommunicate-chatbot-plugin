d5830b12cbed9f4ba58628be7474d465d60a6fb7
panwaranuj01 <anuj@kommunicate.io>
2023-03-15 17:48:08 +0530

    update the react version.

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import Kommunicate from 'kommunicate-widget'
-import 'kommunicate-widget/dist/index.css'
+import './index.css'
 
 Kommunicate.init("23a6d30a59d247fa64605501f58074ea3", {environment : 'test'})
 
 const App = () => {
-  return <div>asdf</div>
+  return <div>Testing Kommunicate widget</div>
 }
 
 export default App
