875fe7b8cdc23b78b9a007ce9975e00d89bba54a
panwaranuj01 <anuj@kommunicate.io>
2023-08-11 12:13:05 +0530

    added the semicolons

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export interface Window {
-  kommunicate?: any
+  kommunicate?: any;
 }
 export interface Options {
-  [key: string]: any
+  [key: string]: any;
 }
 
 /**
@@ -11,63 +11,63 @@ export interface Options {
  */
 
 export interface KommunicateType {
-  init(appId: string, options?: Options): void
+  init(appId: string, options?: Options): void;
 }
 
 // eslint-disable-next-line no-undef
-export const appWindow: Window & typeof globalThis = window
+export const appWindow: Window & typeof globalThis = window;
 
 const Kommunicate: KommunicateType = {
   init: (appId, options) => {
-    let environment: string
-    let scriptSource = 'https://widget.kommunicate.io/v2/kommunicate.app'
+    let environment: string;
+    let scriptSource = 'https://widget.kommunicate.io/v2/kommunicate.app';
     if (!appId) {
-      console.info('Please provide appId to initialize the widget')
-      return
+      console.info('Please provide appId to initialize the widget');
+      return;
     }
     if (typeof appId !== 'string') {
-      console.info('AppId should be string')
-      return
+      console.info('AppId should be string');
+      return;
     }
     const settings = {
       appId: appId,
       popupWidget: true,
       automaticChatOpenOnNavigation: true
-    }
+    };
 
     if (isValidObject(options)) {
       // removing the environment variable
-      const { environment, ...option } = options
-      Object.assign(settings, option)
+      const { environment, ...option } = options;
+      Object.assign(settings, option);
     }
 
     switch (environment) {
       case 'test':
-        scriptSource = 'https://widget-test.kommunicate.io/v2/kommunicate.app'
-        break
+        scriptSource = 'https://widget-test.kommunicate.io/v2/kommunicate.app';
+        break;
       case 'local':
-        scriptSource = 'http://localhost:3030/v2/kommunicate.app'
-        break
+        scriptSource = 'http://localhost:3030/v2/kommunicate.app';
+        break;
       default:
-        scriptSource = 'https://widget.kommunicate.io/v2/kommunicate.app'
-        break
+        scriptSource = 'https://widget.kommunicate.io/v2/kommunicate.app';
+        break;
     }
-    ;(function (d, m: any) {
-      const kommunicateSettings = settings
-      const s = document.createElement('script')
-      s.type = 'text/javascript'
-      s.async = true
-      s.src = scriptSource
-      const h = document.getElementsByTagName('head')[0]
-      h.appendChild(s)
-      appWindow.kommunicate = m
-      m._globals = kommunicateSettings
-    })(document, appWindow.kommunicate || {})
+    (function (d, m: any) {
+      const kommunicateSettings = settings;
+      const s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = scriptSource;
+      const h = document.getElementsByTagName('head')[0];
+      h.appendChild(s);
+      appWindow.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, appWindow.kommunicate || {});
   }
-}
+};
 
-export default Kommunicate
+export default Kommunicate;
 
 function isValidObject(object: any): boolean {
-  return object && Object.keys(object).length && object.constructor === Object
+  return object && Object.keys(object).length && object.constructor === Object;
 }
